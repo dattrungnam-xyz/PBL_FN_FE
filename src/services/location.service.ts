90bbf76f4c9f835b3cfe668db1bbd0fb48867d8e
dattrungnam <datvtrg0510@gmail.com>
2025-06-15 22:19:27 +0700

    add service for location

--- src/services/location.service.ts
+++ src/services/location.service.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from "../axios";
 
-const BASE_URL = "https://open.oapi.vn/location";
+// const BASE_URL = "https://open.oapi.vn/location";
 
 export const getProvinces = async () => {
-  const response = await axios.get(`${BASE_URL}/provinces`, {
+  const response = await axios.get(`/provinces`, {
     params: {
       page: 0,
       size: 1000,
@@ -13,7 +13,7 @@ export const getProvinces = async () => {
 };
 
 export const getDistricts = async (provinceId: string) => {
-  const response = await axios.get(`${BASE_URL}/districts/${provinceId}`, {
+  const response = await axios.get(`/districts/${provinceId}`, {
     params: {
       page: 0,
       size: 1000,
@@ -23,26 +23,11 @@ export const getDistricts = async (provinceId: string) => {
 };
 
 export const getWards = async (districtId: string) => {
-  const response = await axios.get(`${BASE_URL}/wards/${districtId}`, {
+  const response = await axios.get(`/wards/${districtId}`, {
     params: {
       page: 0,
       size: 1000,
     },
   });
   return response.data.data;
-};
-
-export const getProvinceName = async (provinceId: string) => {
-  const response = await axios.get(`${BASE_URL}/provinces/${provinceId}`);
-  return response.data.data.name;
-};
-
-export const getDistrictName = async (districtId: string) => {
-  const response = await axios.get(`${BASE_URL}/districts/${districtId}`);
-  return response.data.data.name;
-};
-
-export const getWardName = async (wardId: string) => {
-  const response = await axios.get(`${BASE_URL}/wards/${wardId}`);
-  return response.data.data.name;
 };
